0ee5081fcb9f2c02866ac38209e16deacbfb6038
adrian <profilte82@gmail.com>
2025-05-21 11:00:03 +0100

    Cambios en server

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 // Configuración de conexión a MySQL
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAM
